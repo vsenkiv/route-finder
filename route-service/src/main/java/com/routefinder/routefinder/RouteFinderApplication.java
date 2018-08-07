b049a1c0b341c75cb4aa678f92e09d8fc1e820ad
Volodymyr Senkiv <Volodymr_Senkiv@epam.com>
2018-08-07 15:06:53 +0300

    Added other services

---
+++ route-service/src/main/java/com/routefinder/routefinder/RouteFinderApplication.java
@@ -0,0 +1,17 @@
+package com.routefinder.routefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class RouteFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RouteFinderApplication.class, args);
+    }
+}
