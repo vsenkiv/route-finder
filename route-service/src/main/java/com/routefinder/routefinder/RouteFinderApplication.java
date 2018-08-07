e2037cce49fd738e22f608d926f40cf3965fe51b
Volodymyr Senkiv <Volodymr_Senkiv@epam.com>
2018-08-07 15:50:29 +0300

    Delete unused imports

--- route-service/src/main/java/com/routefinder/routefinder/RouteFinderApplication.java
+++ route-service/src/main/java/com/routefinder/routefinder/RouteFinderApplication.java
@@ -2,9 +2,6 @@ package com.routefinder.routefinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 public class RouteFinderApplication {
