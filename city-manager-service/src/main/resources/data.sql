6564f48cc03ea967cdb78387b5638642752508c3
Volodymyr Senkiv <Volodymr_Senkiv@epam.com>
2018-08-07 22:42:29 +0300

    Added API Gateway

--- city-manager-service/src/main/resources/data.sql
+++ city-manager-service/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO cityroute(city, destiny_city, departure_time, arrival_time) VALUES ('A', 'B', '2018-08-07 00:10:00.000000', '2018-08-07 00:20:00.000000');
-INSERT INTO cityroute(city, destiny_city, departure_time, arrival_time) VALUES ('A', 'C', '2018-08-07 00:25:00.000000', '2018-08-07 00:40:00.000000');
+INSERT INTO cityroute(city, destiny_city, departure_time, arrival_time) VALUES ('A', 'C', '2018-08-07 00:25:00.000000', '2018-08-07 00:26:00.000000');
 INSERT INTO cityroute(city, destiny_city, departure_time, arrival_time) VALUES ('B', 'C', '2018-08-07 00:26:00.000000', '2018-08-07 00:28:00.000000');
 INSERT INTO cityroute(city, destiny_city, departure_time, arrival_time) VALUES ('B', 'D', '2018-08-07 00:56:00.000000', '2018-08-07 00:59:00.000000');
 INSERT INTO cityroute(city, destiny_city, departure_time, arrival_time) VALUES ('C', 'D', '2018-08-07 00:33:00.000000', '2018-08-07 00:40:00.000000');
